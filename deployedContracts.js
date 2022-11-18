bb2c5d65fdd76ca716a8d52f9ee9f12758f33798
Gaurav Kelkar <gauravkelkar35@gmail.com>
2022-11-18 11:17:29 +0530

    Changed token name and symbol to Aurora Token (AURA)

--- deployedContracts.js
+++ deployedContracts.js
@@ -41,13 +41,26 @@ const contracts = [
 		//implemented faucet code
 		id: 4,
 		name: "fancyBanana token",
-		symbol: "FAPPL",
+		symbol: "FBNNA",
 		totalSupply: 100000000,
 		creationAccount: {
 			address: "0x54fCe65792cbC7ad71c7220D6461D023A4d413a6",
 			key: `0x${process.env.ACC1_PRIVATE_KEY}`,
 		},
 		address: "0x4545761717E1aEB030C99e178968E08A4Ce27B10",
+		type: "ERC20",
+	},
+	{
+		//implemented faucet code
+		id: 5,
+		name: "Aurora token",
+		symbol: "AURA",
+		totalSupply: 100000000,
+		creationAccount: {
+			address: "0x54fCe65792cbC7ad71c7220D6461D023A4d413a6",
+			key: `0x${process.env.ACC1_PRIVATE_KEY}`,
+		},
+		address: "0x37e97312DFbF16dEB489875751a2B982aA17b2cF",
 		type: "ERC20",
 	},
 ];
